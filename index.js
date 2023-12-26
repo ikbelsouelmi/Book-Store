cf2375cafe3fd21d6e767cbd03bf8c8df89eb1f4
ikbelsouelmi <ikbel.s@hotmail.com>
2023-12-26 19:24:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function displayData() {
+ var data = {
+        BookName: $('#n1').val(),
+        Bookauthor: $('#n2').val(),
+        BookPrice: $('#n3').val(),
+        BookCategory: $('#n4').val(),
+    };
+
+console.log("Book Name: " + data.BookName);
+console.log("Book Author: " + data.Bookauthor);
+console.log("Book Price: " + data.BookPrice);
+console.log("Book Category: " + data.BookCategory);
+}
+
+
+
+                   
